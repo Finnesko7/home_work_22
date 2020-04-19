93468d09e16ff9d58456839fc7628045d88702fa
sergey_atamanyuk <sergey.a@uvision.co.il>
2020-04-19 17:51:52 +0300

    home work

--- src/assest/js/finnesko.js
+++ src/assest/js/finnesko.js
@@ -2,6 +2,19 @@ var test = function () {
     console.log("Finnesko 2 test 123");
 };
 
-module.exports = {
-    test: test
+const renderUsers  = users => {
+    let app = document.getElementById('users');
+    console.log(app);
+    users.forEach(function (user) {
+        let div = document.createElement('div');
+        div.innerText = user.login;
+        div.setAttribute('class', 'user');
+        app.append(div);
+    });
 };
+
+module.exports = {
+    test: test,
+    renderUsers: renderUsers
+};
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-var finnesko = require('./assest/js/finnesko');
+const finnesko = require('./assest/js/finnesko');
+const axios = require('axios');
 import './assest/css/main.css';
-finnesko.test();
+
+
+const api = axios.create({
+    baseURL: 'http://localhost:3000', // на экспресе поставил cors чтобы разрешил делать запрос
+    timeout: 1000
+});
+
+(async () => {
+    let number = Math.floor(Math.random() * 20);
+    let user = {
+        login: `login ${number}`,
+        name: `name ${number}`
+    };
+
+    api.post('/favorites', user).then(() => {
+        var users = api.get('/favorites').then((response) => {
+            return response;
+        }).catch( (error) => {
+            console.log(error);
+        });
+
+        return users;
+    }).then((data) => {
+        finnesko.renderUsers(data.data);
+        console.log('data:', data);
+    });
+})();
